3e9fee2aa84d4086d0b4526e370140b871d71573
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-17 16:22:17 -0700

    example of a type guard

--- src/app.ts
+++ src/app.ts
@@ -20,3 +20,10 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric; 
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
